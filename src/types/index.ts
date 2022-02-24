714e93bb9ddb83ededdcd6fc2f3d444daadd45fb
yunred <nado0903@gmail.com>
2022-02-24 18:59:47 +0900

    Add: types 폴더 추가, 타입 정리

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+export interface FileType {
+  key: string;
+  thumbnailUrl: string;
+  name: string;
+  size: number;
+}
+export interface SentType {
+  subject: string;
+  content: string;
+  email: string[];
+}
+export interface FetchDataType {
+  created_at: number;
+  key: string | undefined;
+  expires_at: number;
+  download_count: number;
+  count: number;
+  size: number;
+  summary: string;
+  thumbnailUrl: string;
+  files: FileType[];
+  sent?: SentType;
+}
